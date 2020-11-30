a480dd2d545c3bd5b63ff769836111484dfd1ce8
Romanych <andreidanyliuk@gmail.com>
2020-11-30 13:11:53 +0200

    исправлен отступ count, placeholder, js

--- dart/#sources/js/forms.js
+++ dart/#sources/js/forms.js
@@ -1,160 +1,108 @@
-document.addEventListener('DOMContentLoaded', function () {
-   let form = document.getElementById('form');
-   form.addEventListener('submit', formSend);
+
+let form = document.getElementById('form');
+form.addEventListener('submit', formSend);
 
 
-   async function formSend(e) {
-      e.preventDefault();
+async function formSend(e) {
+   e.preventDefault();
 
-      let error = formValidate();
+   let error = formValidate();
 
-      let formData = new FormData(form);
+   let formData = new FormData(form);
 
-      if (error === 0) {
-         form.classList.add('_sending');
+   if (error === 0) {
+      form.classList.add('_sending');
 
-         let response = await fetch('sendmail.php', {
-            method: 'POST',
-            body: formData
-         });
+      let response = await fetch('sendmail.php', {
+         method: 'POST',
+         body: formData
+      });
 
-         if (response.ok) {
-            let result = await response.json();
-            alert(result.message);
-            form.reset();
-            form.classList.remove('_sending');
-         } else {
-            alert('Error');
-            form.classList.remove('_sending');
-         }
-
+      if (response.ok) {
+         let result = await response.json();
+         alert(result.message);
+         form.reset();
+         form.classList.remove('_sending');
       } else {
-         alert('Fill in the required fields');
+         alert('Error');
+         form.classList.remove('_sending');
       }
+
+   } else {
+      alert('Fill in the required fields');
    }
+}
 
-   function formValidate() {
-      let error = 0;
-      let formReq = document.querySelectorAll('._req');
-      for (let index = 0; index < formReq.length; index++) {
-         const input = formReq[index];
-         formRemoveError(input);
+function formValidate() {
+   let error = 0;
+   let formReq = document.querySelectorAll('._req');
+   for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index];
+      formRemoveError(input);
 
 
-         if (input.classList.contains('_email')) {
-            if (emailTest(input)) {
-               formAddError(input);
-               formRemoveSuccess(input);
-               error++;
-            }
-         } else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
+      if (input.classList.contains('_email')) {
+         if (emailTest(input)) {
             formAddError(input);
+            formRemoveSuccess(input);
             error++;
-         } else {
-            if (input.value === '') {
+         }
+      } else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
+         formAddError(input);
+         error++;
+      } else {
+         if (input.value === '') {
+            formAddError(input);
+            formRemoveSuccess(input)
+            error++;
+         }
+      }
+   }
+   return error;
+}
+
+function inputSuccess() {
+   let inputs = form.querySelectorAll('._req');
+   let er = 0;
+   for (let index = 0; index < inputs.length; index++) {
+      const input = inputs[index];
+      input.addEventListener("input", function () {
+         if (input.validity.valid) {
+            if (input.classList.contains('_error')) {
+               er = 1
+               formRemoveError(input);
+            }
+            formAddSuccess(input);
+         }
+         if (input.value === '' || input.value == '[A-Za-z]') {
+            formRemoveSuccess(input);
+            if (er) {
                formAddError(input);
-               formRemoveSuccess(input)
-               error++;
             }
          }
-      }
-      return error;
+      }, false);
    }
+}
+inputSuccess();
 
-   function inputSuccess() {
-      let inputs = form.querySelectorAll('._req');
-      let er = 0;
-      for (let index = 0; index < inputs.length; index++) {
-         const input = inputs[index];
-         input.addEventListener("input", function () {
-            if (input.validity.valid) {
-               if (input.classList.contains('_error')) {
-                  er = 1
-                  formRemoveError(input);
-               }
-               formAddSuccess(input);
-            }
-            if (input.value === '' || input.value == '[A-Za-z]') {
-               formRemoveSuccess(input);
-               if (er) {
-                  formAddError(input);
-               }
-            }
-         }, false);
-      }
-   }
-   inputSuccess();
+function formAddError(input) {
+   input.parentElement.classList.add('_error');
+   input.classList.add('_error');
+}
 
-   function formAddError(input) {
-      input.parentElement.classList.add('_error');
-      input.classList.add('_error');
-   }
+function formRemoveError(input) {
+   input.parentElement.classList.remove('_error');
+   input.classList.remove('_error');
+}
 
-   function formRemoveError(input) {
-      input.parentElement.classList.remove('_error');
-      input.classList.remove('_error');
-   }
+function emailTest(input) {
+   return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
 
-   function emailTest(input) {
-      return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-   }
+function formAddSuccess(input) {
+   input.classList.add('_success');
+}
 
-   function formAddSuccess(input) {
-      input.classList.add('_success');
-   }
-
-   function formRemoveSuccess(input) {
-      input.classList.remove('_success');
-   }
-
-   // placeholder======================================
-   function placeHolder() {
-      const input = form.querySelectorAll('input[data-txt]');
-      const textArea = form.querySelectorAll('textarea[data-txt]');
-      if (input) {
-         for (let i = 0; i < input.length; i++) {
-            let elem = input[i];
-            addPlaceholder(elem);
-
-            elem.addEventListener('focus', function () {
-               removePlaceholder(elem);
-            });
-
-            elem.addEventListener('blur', function () {
-               if (elem.value == '') {
-                  addPlaceholder(elem);
-               }
-            });
-         }
-      }
-      if (textArea) {
-         for (let i = 0; i < textArea.length; i++) {
-            let elem = textArea[i];
-            addPlaceholder(elem);
-
-            elem.addEventListener('focus', function () {
-               removePlaceholder(elem);
-            });
-
-            elem.addEventListener('blur', function () {
-               if (elem.value === '') {
-                  addPlaceholder(elem);
-               }
-            });
-         }
-      }
-   }
-   placeHolder();
-
-   function addPlaceholder(elem) {
-      let dataTxt = elem.getAttribute('data-txt');
-      elem.insertAdjacentHTML('afterend', `<div class="placeholder">${dataTxt}</div>`);
-   }
-
-   function removePlaceholder(elem) {
-      if (elem.nextSibling) {
-         elem.nextSibling.remove();
-      }
-   }
-
-});
+function formRemoveSuccess(input) {
+   input.classList.remove('_success');
+}
--- dart/#sources/js/spoiler.js
+++ dart/#sources/js/spoiler.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
-   $('.spoiler__title').click(function (event) {
-      if ($('.spoiler__body').hasClass('one')) {
-         $('.spoiler__title').not($(this)).removeClass('active');
-         $('.spoiler__content').not($(this).next()).slideUp(300);
-      }
-      $(this).toggleClass('active').next().slideToggle(300);
-   });
+
+$('.spoiler__title').click(function (event) {
+   if ($('.spoiler__body').hasClass('one')) {
+      $('.spoiler__title').not($(this)).removeClass('active');
+      $('.spoiler__content').not($(this).next()).slideUp(300);
+   }
+   $(this).toggleClass('active').next().slideToggle(300);
 });
+
